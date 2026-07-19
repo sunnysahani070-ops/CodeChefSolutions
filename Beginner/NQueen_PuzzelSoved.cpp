3e82bc6db3064027cf2e62ca5559684509357b02
sunny <sunnysahani070@gmail.com>
2026-07-19 20:28:37 +0530

    Add NQueen_PuzzelSoved.cpp solution

---
+++ Beginner/NQueen_PuzzelSoved.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    cout << round(pow(0.143 * n, n)) << endl;
+	}
+
+}
